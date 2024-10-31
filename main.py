a2adeb4c2bccafd28a3d1dda159eb84143fcaf23
Chase-Outman <Chaser5114@gmail.com>
2024-10-31 11:52:24 -0500

    Added collision detection to asteroids and the player, ends the game if player touches an asteroid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import *
 from asteroidfield import AsteroidField
@@ -34,6 +35,11 @@ def main():
 
         for u in updatable:
             u.update(dt)
+
+        for a in asteroids:
+            if a.collision(player):
+                print("Game Over!")
+                sys.exit()
 
         screen.fill("black")
 
